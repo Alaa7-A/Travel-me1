8d1a9eecdb7d241021585383f46e0c89d51811ab
alaaalzhoury <alaaalzhore3@gmail.com>
2021-05-23 16:29:36 +0300

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,22 @@
+import '../sass/style.scss';
 import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
-import '../sass/style.scss';
 import '../css/style.css';
 import 'jquery/dist/jquery';
 import 'popper.js/dist/popper';
-import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/js/all';
+
+$(document).ready(function(){
+    $("a.scroll").on('click', function(event) {
+ 
+         var hash = this.hash;
+ 
+         $('html, body').animate({scrollTop: $(hash).offset().top - 100}, 800, function(){});
+     
+     });
+     $('.timer').countTo();
+
+     $(function () {
+      $("#commentForm").validate();
+  });
+ });
